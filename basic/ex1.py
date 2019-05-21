297966d143cdc30bf28f15434fd3c971f7f9f0fc
LuoMo92 <810754420@qq.com>
2019-05-21 17:06:26 +0800

    添加演示文档

---
+++ basic/ex1.py
@@ -0,0 +1,6 @@
+PI = 3.14159
+def add(x,y):
+    return x+y
+print(__name__)
+x,y = 2,3
+print(add(x,y))
---
+++ line-alg/1.3_homework.py
@@ -0,0 +1,11 @@
+"""
+Python求向量的范数
+使用对角矩阵对向量进行缩放
+"""
+
+# In[]:
+# 定义一个一维数组作为向量，包括两个元素
+import numpy as np
+a = np.array([2,3])
+print(a.shape)
+
